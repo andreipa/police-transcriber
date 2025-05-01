a72fb2c1c694518b9e75d8cf1ee92aa2c21a344d
Andrei Andrade <a.andrade@cf-partners.com>
2025-05-01 13:09:53 -0300

    add tests; implement test cases for configuration management in test_config.py

--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-#  Copyright (c) 2025. TechDev Andrade Ltda.
-#  All rights reserved.
-#  This source code is the intellectual property of TechDev Andrade Ltda and is intended for private use, research, or internal projects only. Redistribution and use in source or binary forms are not permitted without prior written permission.
